31b3e4626a5d5b21456ee13e58280ae4f391550f
ahmadimran <ahmadimran8177@gmail.com>
2025-04-08 18:03:47 +0500

    Added mids programs

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
-#include <iostream>
+#include "./console.h"
 using namespace std;
+
+class Car
+{
+private:
+    string name = "Lamborghini";
+    string model = "911";
+
+public:
+    string getCarName()
+    {
+        return name;
+    }
+};
 
 int main()
 {
-    int n = 8;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    string newCarArr[2] = {"Toyota", "Cross"};
+
+    Car newCarObj;
+
+    console.output(newCarObj.getCarName(), true);
+    console.output(newCarArr[0], true);
     return 0;
 }
