6ac047890837ec27a6a0ac35a5f13aae163bc074
ahmadimran <ahmadimran8177@gmail.com>
2025-04-11 19:52:23 +0500

    clean:removed extra files

--- main.cpp
+++ main.cpp
@@ -1,26 +1,8 @@
-#include "./console.h"
+#include <iostream>
 using namespace std;
-
-class Car
-{
-private:
-    string name = "Lamborghini";
-    string model = "911";
-
-public:
-    string getCarName()
-    {
-        return name;
-    }
-};
 
 int main()
 {
-    string newCarArr[2] = {"Toyota", "Cross"};
-
-    Car newCarObj;
-
-    console.output(newCarObj.getCarName(), true);
-    console.output(newCarArr[0], true);
+    cout << "Hello from C++ auto watcher!" << endl;
     return 0;
 }
